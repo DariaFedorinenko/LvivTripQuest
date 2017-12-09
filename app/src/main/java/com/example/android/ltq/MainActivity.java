541a1fbb2c7d826d535d87ae2bfffe85aea12f2d
romanfed1999 <romanfed1999@gmail.com>
2017-12-09 21:25:50 +0200

    added ChooseCategory java class

--- app/src/main/java/com/example/android/ltq/MainActivity.java
+++ app/src/main/java/com/example/android/ltq/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.ltq;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startNewButton();
     }
+
+    private void startNewButton(){
+        Button ChooseCategory = (Button) findViewById(R.id.start_new);
+        ChooseCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ChooseCategory.class));
+            }
+        });
+    }
+
+
+
+
 }
