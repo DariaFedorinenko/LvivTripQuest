2d78b804616bf85d7ead8c8a32e9bb1bf38f190b
maslyanyy <maslyanyyandriy@gmail.com>
2017-12-25 19:03:36 +0200

    ftrutturyu

---
+++ app/src/main/java/com/example/android/ltq/profile_ui.java
@@ -0,0 +1,24 @@
+package com.example.android.ltq;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.ltq.profile_ui;
+
+public class profile_ui extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_ui);
+    }
+}
+
+
+
+
+
+
+
+
+
